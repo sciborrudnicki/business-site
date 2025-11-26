14bad910e9aad078bcde52933211224c9a893beb
Ścibor Rudnicki <sciborrudnicki@wvvw.dev>
2025-11-26 17:01:29 +0100

    update.

--- src/stories/button.stories.ts
+++ src/stories/button.stories.ts
@@ -1,29 +1,24 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { fn } from 'storybook/test';
 
-import { ButtonComponent } from './button.component';
+import { Button } from '../app/button/button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
-const meta: Meta<ButtonComponent> = {
+const meta: Meta<Button> = {
   title: 'Example/Button',
-  component: ButtonComponent,
+  component: Button,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: {
-      control: 'color',
-    },
-  },
+  argTypes: {},
   // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#story-args
   args: { onClick: fn() },
 };
 
 export default meta;
-type Story = StoryObj<ButtonComponent>;
+type Story = StoryObj<Button>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {
   args: {
-    primary: true,
     label: 'Button',
   },
 };
